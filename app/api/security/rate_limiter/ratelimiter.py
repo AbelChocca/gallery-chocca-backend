826533a75b22056c7fc041365f0251b97498dcd3
AbelChocca <abelchocca1010@gmail.com>
2025-12-22 21:12:33 -0500

    api(security): added rate limiter dependency for endpoints

---
+++ app/api/security/rate_limiter/ratelimiter.py
@@ -0,0 +1,22 @@
+from fastapi import Request, Depends, HTTPException, status
+from redis import Redis
+
+from typing import Callable, Optional, Awaitable
+
+from app.core.clients.redis_client import get_redis_client
+
+class RateLimiter:
+    def limiter(self, limit = 5, window = 60) -> Callable[[Request], Awaitable[Optional[HTTPException]]]:
+        async def dependency(request: Request, redis_client: Redis = Depends(get_redis_client)) -> Optional[HTTPException]:
+            key = f"rate:{request.client.host}:{request.url.path}"
+            current = await redis_client.incr(key)
+            if current == 1:
+                await redis_client.expire(key, window)
+            if current > limit:
+                return HTTPException(
+                    detail={"message": f"Rate limit exceeded. Try again in {window} seconds."},
+                    status_code=status.HTTP_429_TOO_MANY_REQUESTS
+                )
+        return dependency
+    
+limiter = RateLimiter()
