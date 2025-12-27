26df7a5b19291dc0612845293e52ff3041adb12f
AbelChocca <abelchocca1010@gmail.com>
2025-12-27 18:06:19 -0500

    app(products): added delete key cache on DeleteProductCase to delete category with brand

--- app/application/products/cases/delete_product.py
+++ app/application/products/cases/delete_product.py
@@ -36,6 +36,7 @@ class DeleteProductCase:
         # Cache invalidation
         await self.cache_repo.cache_delete(Product.get_filter_key(id=product_to_delete.id))
         await self.cache_repo.cache_delete(Product.get_filter_key(category=product_to_delete.categoria))
+        await self.cache_repo.cache_delete(Product.get_filter_key(brand=product_to_delete.marca, category=product_to_delete.categoria))
 
         # Del images from cloud service
         images_to_delete: List[str] = product_to_delete.get_all_variants_images_id()
