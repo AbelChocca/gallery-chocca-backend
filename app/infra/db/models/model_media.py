4ef7f5716882d2029080b760ef1d9cb657ba2837
AbelChocca <abelchocca1010@gmail.com>
2026-01-07 18:15:29 -0500

    add: image table for simplify products and slides images

---
+++ app/infra/db/models/model_media.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+
+class MediaImageTable(SQLModel, table=True):
+    __tablename__ = "image"
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    image_url: str = Field(nullable=False)
+    alt_text: Optional[str] = Field(default=None)
+
+    owner_type: str = Field(index=True)
+    owner_id: int = Field(index=True)
