f9f7f78a392a86aa4e0fc668ad23ea0bc656381d
AbelChocca <abelchocca1010@gmail.com>
2026-01-09 19:52:29 -0500

    change: improve get_images to be multi-pattern method to get an one or more images

--- app/infra/db/repositories/sqlmodel_image_repository.py
+++ app/infra/db/repositories/sqlmodel_image_repository.py
@@ -1,7 +1,7 @@
 from app.infra.db.repositories.base_repository import BaseRepository
 from app.infra.db.models.model_media import MediaImageTable
 from app.domain.media.entities.image import ImageEntity
-from app.infra.db.exceptions import DatabaseException
+from app.infra.db.exceptions import DatabaseException, ModelNotFound
 
 from sqlmodel import select, col
 from sqlalchemy.exc import SQLAlchemyError
@@ -9,20 +9,44 @@ from sqlalchemy.exc import SQLAlchemyError
 from typing import List, Optional
 
 class PostgresImageRepository(BaseRepository[ImageEntity, MediaImageTable]):
-    async def get_images(self, owner_type: str, owners_id: Optional[List[int]] = None) -> List[ImageEntity]:
+    async def delete_by_id(self, model_id: str):
+        try:
+            statement = select(MediaImageTable).where(MediaImageTable.service_id == model_id)
+            model_db = await self._db_session.exec(statement)
+
+            if not model_db:
+                raise ModelNotFound(f"Model {MediaImageTable.__name__} with id: {model_id} wasn't found, cannot deleted.")
+
+            await self._db_session.delete(model_db)
+            await self._db_session.commit()
+        except SQLAlchemyError as s:
+            await self._db_session.rollback()
+            raise DatabaseException(f"Database error while deleting {MediaImageTable.__name__}.") from s
+    
+    async def get_images(
+            self, 
+            owner_type: str, 
+            multiply: bool = False,
+            owner_id: Optional[List[int] | int] = None
+        ) -> List[ImageEntity] | ImageEntity:
         statement = (
             select(MediaImageTable)
             .where(MediaImageTable.owner_type == owner_type)
         )
-        if owners_id is not None:
-            statement = statement.where(col(MediaImageTable.owner_id).in_(owners_id))
+        if multiply:
+            statement = statement.where(col(MediaImageTable.owner_id).in_(owner_id or []))
 
-        res: List[MediaImageTable] = (await self._db_session.exec(statement)).all()
+            res: List[MediaImageTable] = (await self._db_session.exec(statement)).all()
 
-        return [
-            self._base_mapper.to_entity(image_table)
-            for image_table in res
-        ]
+            return [
+                self._base_mapper.to_entity(image_table)
+                for image_table in res
+            ]
+        statement = statement.where(col(MediaImageTable.owner_id == owner_id))
+
+        res: MediaImageTable = (await self._db_session.exec(statement)).first()
+        return self._base_mapper.to_entity(res)
+
     async def save_many(self, entities: List[ImageEntity]) -> List[ImageEntity]:
         try:
             models: List[MediaImageTable] = [self._base_mapper.to_db_model(entity) for entity in entities]
