588e50ef222605670b12aa9e505bf5ef60b99167
AbelChocca <abelchocca1010@gmail.com>
2026-01-11 22:25:39 -0500

    change: the getter is based on factory repo dependency

--- app/api/dependencies/slides/repo.py
+++ app/api/dependencies/slides/repo.py
@@ -1,11 +1,8 @@
-from app.modules.slide.domain.slide_repository import SlideRepository
-from app.modules.slide.infra.sqlmodel_slide_repository import InfraSlideRepository
-from app.core.log.logger_repository import LoggerRepository
-from app.core.clients.db import get_async_session
-from app.core.log.loguru_logger_repository import get_logger_repo
+from app.infra.db.factory_repository import FactoryRespository
+from app.infra.db.repositories.sqlmodel_slide_repository import PostgresSlideRepository
+from app.api.dependencies.repo import get_fatory_repo
 
-from sqlmodel.ext.asyncio.session import AsyncSession
 from fastapi import Depends
 
-def get_slide_repo(db_sesison: AsyncSession = Depends(get_async_session, scope="function"), logger: LoggerRepository = Depends(get_logger_repo)) -> SlideRepository:
-    return InfraSlideRepository(db_sesison, logger)
+def get_slide_repo(factory: FactoryRespository = Depends(get_fatory_repo)) -> PostgresSlideRepository:
+    return factory.get_slide_repository()
