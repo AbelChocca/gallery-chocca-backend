579ee368029668aca022fbcb333dd76fe82e6df7
AbelChocca <abelchocca1010@gmail.com>
2026-01-12 21:59:42 -0500

    add: schema mapper to mapping the result of calleables

---
+++ app/api/schemas/media/schema_mapper.py
@@ -0,0 +1,14 @@
+from app.domain.media.media_dto import ReadMediaImageDTO
+from app.api.schemas.media.media_schema import ReadImage
+
+class OutputSchemaMapper:
+    @staticmethod
+    def to_schema(dto: ReadMediaImageDTO) -> ReadImage:
+        return ReadImage(
+            image_url=dto.image_url,
+            owner_id=dto.owner_id,
+            service_id=dto.service_id,
+            owner_type=dto.owner_type,
+            id=dto.id,
+            alt_text=dto.alt_text
+        )
