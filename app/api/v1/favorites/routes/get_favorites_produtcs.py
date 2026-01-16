528a45396e46dee54c3016a8cf2ceb200441620e
AbelChocca <abelchocca1010@gmail.com>
2026-01-15 22:11:30 -0500

    feat(favorites): add endpoint to retrieve favorite products by user or session

--- app/api/v1/favorites/routes/get_favorites_produtcs.py
+++ app/api/v1/favorites/routes/get_favorites_produtcs.py
@@ -1,8 +1,10 @@
 from app.api.v1.favorites.favorites_router import router
-from app.api.security.dependencies.sessions import get_user_id
-from app.application.favorites.cases.get_favorite_products import GetFavoriteProducts
+from app.application.favorites.cases.get_favorite_products import GetFavoriteProductsCase
 
 from app.api.schemas.products.schema import ProductRead
+from app.api.schemas.products.schema_mapper import OutputSchemaMapper
+from app.api.dependencies.favorites.case_depends import get_favorites_products_case
+from app.api.dependencies.favorites.resolvers.favortire_owner import get_favorite_owner, FavoriteOwner
 
 from fastapi import Depends, status
 from typing import List, Annotated
@@ -14,8 +16,18 @@ from typing import List, Annotated
     summary="Get your favorite products based on you user id or anon session id"
 )
 async def get_favorites_products(
-    user_id: Annotated[int | None, Depends(get_user_id)],
-    case: Annotated[GetFavoriteProducts, Depends]
+    owner: Annotated[FavoriteOwner, Depends(get_favorite_owner)],
+    case: Annotated[GetFavoriteProductsCase, Depends(get_favorites_products_case)]
 ) -> List[ProductRead]:
-    if user_id:
-        return 
+    if owner.is_user:
+        favorite_products = await case.execute(user_id=owner.user_id)
+        return [
+            OutputSchemaMapper.to_read_schema(product)
+            for product in favorite_products
+        ]
+    
+    favorite_products = await case.execute(session_id=owner.session_id)
+    return [
+            OutputSchemaMapper.to_read_schema(product)
+            for product in favorite_products
+        ]
