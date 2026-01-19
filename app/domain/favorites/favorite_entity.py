da492f8933c62f77bc565bf5e8faa98ae36ea94d
AbelChocca <abelchocca1010@gmail.com>
2026-01-18 22:23:49 -0500

    doamin(favorites): making attributes optional and insert condition of datetime

--- app/domain/favorites/favorite_entity.py
+++ app/domain/favorites/favorite_entity.py
@@ -1,15 +1,17 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Optional
 
 class FavoriteEntity:
     def __init__(
             self,
-            user_id: int,
             product_id: int,
-            created_at: datetime,
+            created_at: Optional[datetime] = None,
+            user_id: Optional[int] = None,
+            session_id: Optional[str] = None,
             id: Optional[int] = None
             ):
-        self.user_id: int = user_id
+        self.user_id: Optional[int] = user_id
+        self.session_id: Optional[str] = session_id
         self.product_id: int = product_id
-        self.created_at: datetime = created_at
+        self.created_at: datetime = created_at or datetime.now(timezone.utc)
         self.id: Optional[int] = id
