4afcc85db0b7d8f0c6fe875bce570635f5c9b090
AbelChocca <abelchocca1010@gmail.com>
2026-03-15 08:27:56 -0500

    refactor(slug): rename slug service

--- app/shared/services/slug/slugify_service.py
+++ app/shared/services/slug/slugify_service.py
@@ -1,9 +1,9 @@
 from app.shared.services.slug.protocol import SlugProtocol
 from slugify import slugify
 
-class SlugifyRepository(SlugProtocol):
+class SlugService(SlugProtocol):
     def generate(self, value: str) -> str:
         return slugify(value)
 
 def get_slugify_service() -> SlugProtocol:
-    return SlugifyRepository()
+    return SlugService()
