9e1fe7c754075ad7eb04402fc694d0ba0556d2eb
AbelChocca <abelchocca1010@gmail.com>
2026-03-26 10:12:06 -0500

    application(media): add principal cases of media module

--- app/application/media/cases/delete_image.py
+++ app/application/media/cases/delete_image.py
@@ -1,13 +1,37 @@
-from app.domain.media.protocol import MediaProtocol
-from typing import Dict
+from app.application.media.service import MediaService
+from app.core.app_exception import AppException
+from app.infra.saga_service import SagaService
 
 class DeleteImageCase:
     def __init__(
             self,
-            repo: MediaProtocol
+            media_service: MediaService,
+            saga_service: SagaService
             ):
-        self.repo = repo
+        self.media_service = media_service
+        self._saga_service = saga_service
 
-    def execute(self, public_id: str) -> Dict[str, str]:
-        self.repo.delete_image(public_id)
-        return {"meesage": "The Image was deleted successfully"}
+    async def execute(self, public_id: str) -> dict[str, str]:
+        try:
+            self._saga_service.add_step(
+                action=self.media_service.move_image_to_trash,
+                action_name="move_image_to_trash",
+                action_kwargs={
+                    "public_id": public_id
+                },
+                compensation=self.media_service.recover_image_from_trash,
+                compensation_name="recover_image_from_trash",
+                compensation_kwargs={
+                    "public_id": public_id
+                }
+            )
+            await self._saga_service.execute_last()
+            
+            return {"meesage": "The Image was deleted successfully"}
+        except AppException as ae:
+            await self._saga_service.compensate_all()
+
+            if self._saga_service.compensation_errors:
+                ae.context["compensation_errors"] = self._saga_service.compensation_errors
+
+            raise ae
--- app/application/media/cases/upload_image.py
+++ app/application/media/cases/upload_image.py
@@ -1,14 +1,42 @@
-from app.domain.media.protocol import MediaProtocol
+from app.application.media.service import MediaService
 from app.domain.media.media_dto import MediaImageDTO
+from app.core.app_exception import AppException
+from app.infra.saga_service import SagaService
 
 from typing import BinaryIO
 
 class UploadImageCase:
     def __init__(
             self,
-            repo: MediaProtocol
+            media_service: MediaService,
+            saga_service: SagaService
             ):
-        self.repo = repo
+        self.media_service = media_service
+        self._saga_service = saga_service
 
-    def execute(self, file: BinaryIO, folder: str) -> MediaImageDTO:
-        return self.repo.upload_image(file, folder)
+    async def execute(self, file: BinaryIO, folder: str) -> MediaImageDTO:
+        try:
+            self._saga_service.add_step(
+                action=self.media_service.upload_image,
+                action_name="upload_image",
+                action_kwargs={
+                    "image_resource": file,
+                    "folder": folder
+                }
+            )
+            res: MediaImageDTO = await self._saga_service.execute_last()
+            self._saga_service.set_last_step_compensation(
+                compesation=self.media_service.move_image_to_trash,
+                compensation_name="move_image_to_trash",
+                compensation_kwargs={
+                    "public_id": res.public_id
+                }
+            )
+            
+        except AppException as ae:
+            await self._saga_service.compensate_all()
+
+            if self._saga_service.compensation_errors:
+                ae.context["compensation_errors"] = self._saga_service.compensation_errors
+
+            raise ae
