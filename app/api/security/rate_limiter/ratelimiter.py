a25c47a14ff9f9d96486502cf2f62a7ecda18dd9
AbelChocca <abelchocca1010@gmail.com>
2026-03-26 10:22:06 -0500

    api(security): patch from HTTPException to TooManyRequests to handle exception consistent

--- app/api/security/rate_limiter/ratelimiter.py
+++ app/api/security/rate_limiter/ratelimiter.py
@@ -1,21 +1,26 @@
-from fastapi import Request, Depends, HTTPException, status
+from fastapi import Request, Depends, HTTPException
 from redis.asyncio import Redis
 
-from typing import Callable, Optional, Awaitable
+from typing import Callable, Awaitable
 
 from app.infra.cache.config import get_redis_client
+from app.api.security.exceptions import TooManyRequests
 
 class RateLimiter:
-    def limiter(self, limit = 5, window = 60) -> Callable[[Request], Awaitable[Optional[HTTPException]]]:
-        async def dependency(request: Request, redis_client: Redis = Depends(get_redis_client)) -> Optional[HTTPException]:
+    def limiter(self, limit = 5, window = 60) -> Callable[[Request], Awaitable[HTTPException | None]]:
+        async def dependency(request: Request, redis_client: Redis = Depends(get_redis_client)) -> HTTPException | None:
             key = f"rate:{request.client.host}:{request.url.path}"
             current = await redis_client.incr(key)
             if current == 1:
                 await redis_client.expire(key, window)
             if current > limit:
-                raise HTTPException(
-                    detail={"message": f"Rate limit exceeded. Try again in {window} seconds."},
-                    status_code=status.HTTP_429_TOO_MANY_REQUESTS
+                 raise TooManyRequests(
+                    f"Rate limit exceeded. Try again in {window} seconds.",
+                    {
+                        "limit": limit, 
+                        "window": window, 
+                        "key": key
+                    }
                 )
         return dependency
     
