2871401f0b68aad999357e53bb7c259218f96d81
AbelChocca <abelchocca1010@gmail.com>
2026-03-25 22:02:06 -0500

    core: add constants

---
+++ app/core/constants/color_families.py
@@ -0,0 +1,50 @@
+COLOR_FAMILY_MAP: dict[str, tuple[str, ...]] = {
+    # Neutros
+    "black": (
+        "black",
+    ),
+    "white": (
+        "white",
+    ),
+    "gray": (
+        "gray_light",
+        "gray_dark",
+        "blue_gray",
+        "steel_blue",
+        "gray",
+    ),
+    "brown_beige": (
+        "beige",
+        "khaki",
+        "brown",
+        "camel",
+        "sand"
+    ),
+
+    # Azules (especialmente jeans)
+    "blue": (
+        "blue",
+        "blue_light",
+        "blue_royal",
+        "blue_dark",
+        "navy",
+    ),
+
+    # Verdes
+    "green": (
+        "military_green",
+        "olive",
+        "forest_green",
+        "green",
+        "green_light",
+        "olive_brown",
+        "stone_green"
+    ),
+
+    # Colores fuertes
+    "red": (
+        "red",
+        "wine",
+        "burgundy"
+    ),
+}
