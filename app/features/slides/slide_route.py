26a8b8703d622a5a74c4dd03c3b75ea30a7e4c0a
AbelChocca <140349180+AbelChocca@users.noreply.github.com>
2026-06-03 19:52:04 -0500

    feat: update slide route prefix to remove versioning

--- app/features/slides/slide_route.py
+++ app/features/slides/slide_route.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/v1/slides", tags=["slides","v1"])
+router = APIRouter(prefix="/slides", tags=["slides"])
 
 from app.features.slides.routes import delete_slide, get_slides, publish_slide, toggle_session, update_positions, update_slide
