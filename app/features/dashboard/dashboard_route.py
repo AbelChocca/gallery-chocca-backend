93b69064ad4ceda073e94dd374f5325157269a35
AbelChocca <140349180+AbelChocca@users.noreply.github.com>
2026-06-03 20:01:41 -0500

    fix(dashboard): update API router prefix to remove versioning

--- app/features/dashboard/dashboard_route.py
+++ app/features/dashboard/dashboard_route.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/v1/dashboard", tags=['dashboard'])
+router = APIRouter(prefix="/dashboard", tags=['dashboard'])
 
 from app.features.dashboard.routes import overview
 
