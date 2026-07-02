7f696959c775d6049f9be1e9e81085e1a20db44a
AbelChocca <140349180+AbelChocca@users.noreply.github.com>
2026-07-01 20:18:30 -0500

    feat(tests): add comprehensive tests for inventory and material services, including CRUD operations and stock management

---
+++ tests/integration/reports/test_excel_invetory_report.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+from app.features.inventory.types import (
+    InventoryOwnerType
+)
+
+from app.features.reports.dto import (
+    GenerateInventoryMovementReportCommand
+)
