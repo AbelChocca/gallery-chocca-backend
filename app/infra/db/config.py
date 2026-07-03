6b65ee78de38b1c16d4b959797c304c0ce760a46
AbelChocca <140349180+AbelChocca@users.noreply.github.com>
2026-07-02 21:29:29 -0500

    feat(db): refactor database URL configuration into a function for better flexibility

--- app/infra/db/config.py
+++ app/infra/db/config.py
@@ -1,20 +1,26 @@
 from sqlmodel import SQLModel
-from sqlalchemy.ext.asyncio.engine import URL
+from sqlalchemy.engine import URL
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncEngine, AsyncSession
 from app.core.settings.pydantic_settings import settings
 
-DATABASE_URL = URL.create(
-    drivername="postgresql+asyncpg",
-    username=settings.POSTGRES_USER,
-    password=settings.POSTGRES_PASSWORD,
-    host=settings.POSTGRES_HOST,
-    port=5432,
-    database=settings.POSTGRES_DB,
-    query={"ssl": "require"}
-)
+def get_db_url():
+    ssl_mode = {}
+
+    if settings.ENV == "prod":
+        ssl_mode = {"ssl": "require"}
+
+    return URL.create(
+        drivername="postgresql+asyncpg",
+        username=settings.POSTGRES_USER,
+        password=settings.POSTGRES_PASSWORD,
+        host=settings.POSTGRES_HOST,
+        port=5432,
+        database=settings.POSTGRES_DB,
+        query=ssl_mode
+    )
 
 
-engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=False, pool_size=10, max_overflow=5)
+engine: AsyncEngine = create_async_engine(get_db_url(), echo=False, pool_size=10, max_overflow=5)
 
 async_session_factory = async_sessionmaker(
     engine,
