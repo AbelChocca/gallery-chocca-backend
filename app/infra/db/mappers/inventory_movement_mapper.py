5785459d1a0f34c8675e7f1449e29159f112af6c
AbelChocca <140349180+AbelChocca@users.noreply.github.com>
2026-07-24 07:59:13 -0500

    refactor(inventory): update InventoryMovementMapper to include location and reference fields

--- app/infra/db/mappers/inventory_movement_mapper.py
+++ app/infra/db/mappers/inventory_movement_mapper.py
@@ -1,6 +1,6 @@
 from app.infra.db.mappers.base_mapper import BaseMapper
 from app.features.inventory.inventory_movement_entity import InventoryMovement
-from app.infra.db.models.model_inventory_movement import InventoryMovementTable
+from app.features.inventory.models.inventory_movement import InventoryMovementTable
 
 class InventoryMovementMapper(BaseMapper[InventoryMovement, InventoryMovementTable]):
     @staticmethod
@@ -8,7 +8,10 @@ class InventoryMovementMapper(BaseMapper[InventoryMovement, InventoryMovementTab
         if existing_model is not None:
             existing_model.new_stock = entity.new_stock
             existing_model.previous_stock = entity.previous_stock
+            existing_model.location_id = entity.location_id
             existing_model.quantity = entity.quantity
+            existing_model.reference_type=entity.reference_type
+            existing_model.reference_id=entity.reference_id
             existing_model.reason = entity.reason
             existing_model.type = entity.type
             return existing_model
@@ -16,10 +19,14 @@ class InventoryMovementMapper(BaseMapper[InventoryMovement, InventoryMovementTab
             owner_name=entity.owner_name,
             owner_id=entity.owner_id,
             owner_code=entity.owner_code,
+            location_id=entity.location_id,
             owner_type=entity.owner_type,
             type=entity.type,
             quantity=entity.quantity,
             previous_stock=entity.previous_stock,
+            reference_type=entity.reference_type,
+            reference_id=entity.reference_id,
+            performed_by=entity.performed_by,
             new_stock=entity.new_stock,
             reason=entity.reason,
             created_at=entity.created_at
@@ -32,6 +39,10 @@ class InventoryMovementMapper(BaseMapper[InventoryMovement, InventoryMovementTab
             owner_code=model.owner_code,
             owner_name=model.owner_name,
             owner_type=model.owner_type,
+            location_id=model.location_id,
+            reference_type=model.reference_type,
+            reference_id=model.reference_id,
+            performed_by=model.performed_by,
             type=model.type,
             quantity=model.quantity,
             previous_stock=model.previous_stock,
